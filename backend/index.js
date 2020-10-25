735430c0f67458a1097569556b7d38155a5e3db8
Marcelo Saad <mc.saad@hotmail.com>
2020-10-25 01:45:07 -0300

    Segunda Versão: Adicionado deck de Cartas, porém ainda com bug no load

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,17 @@
     #4 Step: Generate our package.json ---> npm init -y
     #5 Step (optional): Create a nodemon script (start: "nodemon") on our package.json so we can use the bash command: npm start
 */
-
 //Let's begin!
+
+
+
+
 //Variables
 const axios = require("axios");
 const express = require("express");
 const internalApp = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const e = require("express");
 const apiUrl = "https://pokeapi.co/api/v2/";
 class pokemon {
     constructor (name, id, icon, sprite, types) {
@@ -29,68 +31,76 @@ class pokemon {
     }
 };
 const varList = {
-    totalPokemons: "0", //house the total amount of known pokemons
-    offset: "0",        //the pokemon ID that will start the list
-    size: "9",          //the list size
-    next: null,         //store the next list (kindly provided by pokeAPI itself)
-    previous: null,     //store the previous list (kindly provided by pokeAPI itself)
-    dexList: ["start"]         //list of pokemon class variables. ID from (offset) until (offset + size)
+    totalPokemons: 0, //house the total amount of known pokemons
+    offset: 0,        //the pokemon ID that will start the list
+    size: 18,          //the list size
+    next: 18,         //store the next list (kindly provided by pokeAPI itself)
+    previous: 0,     //store the previous list (kindly provided by pokeAPI itself)
+    cardDeck: []         //Array of pokemon class Cards
 }
-
-//CODE
 internalApp.use(cors());
 internalApp.use(bodyParser.json());
 internalApp.use(bodyParser.urlencoded({ extended:true }));
 
-// async function listFill () {    //async function that will update varList and fill varList.dexList 
-//     console.log(`Entrei em listFill, recebendo a seguinte varList inicial:\n${varList}`);
-    
-//     //caso seja a primeira vez que carrego a página, crie uma lista inicial (IDs 1 até 19)
-//     if (dexList[0] === "start") {
-//         await axios.get(`${apiUrl}pokemon?limit=${size}&offset=${offset}`).then( answer => {
-//             console.log(`Primeira inicialização, recebida lista da API com answer.results.length == ${answer.result.length}`)
-//             varList.next = answer.next;
-//             varList.previous = answer.previous;
-//             answer.result.forEach( (el, index) => {
-//                 varList.dexList[index] = await axios.get(el.url) //ainda está errado, vai me dar o JSON completo -> tenho q tratar com a funcao de criar card!!!!
-//                 ;
-//             })
-//             console.log(totalPokemons);
-//         })
-//         return ;
-//     }
-// }
 
-// internalApp.get("/dexlist", async (req, res) => {
-//     ;
-// })
 
-// The get request below searches for a pokemon provided by the front end, responding with a {pokemon} object 
-internalApp.get("/newsearch", async (req, res) => {
+
+// FUNCTIONS
+async function createCard (req, res, needStringfy) {
+    // Receive a pkmn request, searche pokeAPI, create a card, strigfy [if true], sends either object or JSON
     try {
-        await axios.get(`${apiUrl}pokemon/${req.query.name}/`)
+        await axios.get(`${apiUrl}pokemon/${req}/`)
         .then( answer => {
-            if (req.query.name !== "") {
-                console.log(`API answered with ${req.query.name}'s data.`);
+            if (req !== "") {
+                console.log(`API answered with ${req}'s data.`);
                 let name = answer.data.species.name;
                 let id = answer.data.id;
                 let icon = answer.data.sprites.versions['generation-viii'].icons.front_default;
                 let sprite = answer.data.sprites.front_default;
                 let types = [];
                 answer.data.types.forEach( (el,index) => types[index] = el.type.name )
-                res.send(JSON.stringify(new pokemon(name, id, icon, sprite, types)))
-            }
-            else {
-                throw new Error(`It's blank dude, I need you to provide me something!`);
-            }
+                if (needStringfy === true) {
+                    res.send(JSON.stringify(new pokemon(name, id, icon, sprite, types)))
+                }
+                else { return new pokemon(name, id, icon, sprite, types) }
+            } else { throw new Error(`It's blank dude, I need you to provide me something!`); }
         })
     } catch(error) {
         console.error(error.message);
-        res.send(JSON.stringify("oops"));
+        if (needStringfy === true) { res.send(JSON.stringify("oops")); }
     }
-    
-})
+}
 
 
+async function listFill (ID) {
+    //Get a pkmn ID, define offsets based on ID, update varList with the new deck information 
+    console.log(`Entrei em listFill, recebendo o seguinte ID: ${ID}`);
+    try {
+        await axios.get(`${apiUrl}pokemon?limit=${varList.size}&offset=${ID - ID%varList.size}`)
+        .then( answer => {
+            console.log(`Successfull get in "${apiUrl}pokemon?limit=${varList.size}&offset=${ID}"`)
+            varList.offset = ID - ID%varList.size;
+            varList.totalPokemons = answer.data.count;
+            varList.next = varList.offset + varList.size;
+            varList.previous = varList.offset - varList.size;
+            if (varList.previous < 1) varList.previous = 0;
+            answer.data.results.forEach((el, index) => {
+                varList.cardDeck[index] = el.name
+            })
+            console.log(`listFill finalizado, varList foi atualizada:`)
+            console.log(varList);
+        })
+    }
+    catch (e) { console.log(e.message) }
+    finally { return varList }
+}
 
-internalApp.listen(5000, () => { console.log("Server running on port 5000") });
+internalApp.get("/carddeck", async (req, res) => {
+    await listFill (parseInt(req.query.id))
+    .then (res.send(JSON.stringify(varList)))
+});
+
+
+internalApp.get("/newsearch", (req, res) => createCard (req.query.name, res, true));
+
+internalApp.listen(5000, () => { console.log("Server running on port 5000")});
